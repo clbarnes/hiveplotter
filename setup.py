098a3fdb0d1dbb9a3f94051d2498f253ed13b504
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2014-11-18 11:31:19 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hiveplotter',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/clbarnes/hiveplotter',
+    license='MIT',
+    author='clbarnes',
+    author_email='cbarnes@mrc-lmb.cam.ac.uk',
+    description='A python library for creating hive plots from networkx graphs'
+)
